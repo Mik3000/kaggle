d426b3043d21f29c869158c8e60ef6a298690a6c
MikkoVenhuis <mikko.venhuis@coolblue.nl>
2019-08-21 08:25:01 +0200

    some changes to xgboost: run it with NAs

--- mikko/xgboost.R
+++ mikko/xgboost.R
@@ -11,8 +11,8 @@ library(e1071)
 library(randomForest)
 library(ROCR)
 
-train_transaction = fread("data/train_transaction.csv")
-train_identity = fread("data/train_identity.csv")
+train_transaction = fread("data/train_transaction.csv", na.strings=c("","NA"), stringsAsFactors = TRUE)
+train_identity = fread("data/train_identity.csv", na.strings=c("","NA"), stringsAsFactors = TRUE)
 train = left_join(train_transaction, train_identity)
 rm(train_transaction, train_identity)
 
@@ -20,13 +20,9 @@ rm(train_transaction, train_identity)
 set.seed(1234)
 train = sample_n(train, 5000)
 
-# drop columns with empty cells ("")
-empty_cells = ifelse(train == '', 1, 0) %>% colSums()
-train = train[, which(empty_cells == 0)]   
-
 # drop columns with NA cells
-missing_obs = apply(train, 2, function(col)sum(is.na(col))/length(col))
-train = train[, missing_obs < 0.01]
+#missing_obs = apply(train, 2, function(col) sum(is.na(col)) / length(col))
+#train = train[, missing_obs < 0.01]
 
 #train = sample_n(train, 10000)
 train$isFraud = train$isFraud %>% as.factor()
@@ -34,12 +30,12 @@ train$isFraud = train$isFraud %>% as.factor()
 train %>% group_by(isFraud) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
 
 x = train %>% select(-isFraud)
-y = train %>% select(isFraud) %>% unlist()
 
 param <-  data.frame(nrounds=c(100), max_depth = c(2),eta =c(0.3),gamma=c(0),
                      colsample_bytree=c(0.8),min_child_weight=c(1),subsample=c(1))
 
-xgb_fit <- train(isFraud ~ ., train, method = "xgbTree", metric = "Accuracy", trControl = trainControl(method="none"), tuneGrid = param)
+xgb_fit <- train(isFraud ~ ., train, method = "xgbTree", metric = "Accuracy", 
+                 trControl = trainControl(method = "none"), tuneGrid = param, na.action = na.pass)
 
 pred_xgb <- predict(xgb_fit, newdata = x)
 
