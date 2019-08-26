c7adae793478283578991ee919374fac6685c4f4
MikkoVenhuis <mikko.venhuis@coolblue.nl>
2019-08-26 22:14:16 +0200

    added stratified samping for train + test set and upsampling for train set

--- mikko/xgboost.R
+++ mikko/xgboost.R
@@ -16,29 +16,40 @@ library(Matrix)
 
 train_transaction = fread("data/train_transaction.csv", na.strings=c("","NA"), stringsAsFactors = TRUE)
 train_identity = fread("data/train_identity.csv", na.strings=c("","NA"), stringsAsFactors = TRUE)
-train = left_join(train_transaction, train_identity)
+dataset = left_join(train_transaction, train_identity)
 rm(train_transaction, train_identity)
 
 # train on subset
 set.seed(1234)
-train = sample_n(train, 10000)
+dataset = sample_n(dataset, 100000)
 
-train = map_if(train, is.character, as.factor) %>% as.data.frame()
+dataset = map_if(dataset, is.character, as.factor) %>% as.data.frame()
+
+ind <- createDataPartition(dataset$isFraud, p = .7, list = FALSE)
+train <- dataset[ind, ]
+test  <- dataset[-ind, ]
 
 #train_complete = train[complete.cases(train), ]
+x_train = train %>% select(-isFraud)
+y_train = train %>% select(isFraud) %>% unlist %>% as.factor()
+
+# upsample
+train = upSample(x = x_train, y = y_train, yname = "isFraud")
 
 options(na.action='na.pass')
-train_dummy = sparse.model.matrix(isFraud ~ . -1, train)
+#train_dummy = sparse.model.matrix(isFraud ~ . -1, train)
+#saveRDS(train_dummy, "data/train_dummy.RDS")
+#test_dummy = sparse.model.matrix(isFraud ~ . -1, test)
+saveRDS(test_dummy, "data/test_dummy.RDS")
 
 # drop columns with NA cells
 #missing_obs = apply(train, 2, function(col) sum(is.na(col)) / length(col))
 #train = train[, missing_obs < 0.01]
 
-train %>% group_by(isFraud) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
+test %>% group_by(isFraud) %>% summarise(n = n()) %>% mutate(freq = n / sum(n))
 
-x = train_dummy[ ,1:100]
-y = train %>% select(isFraud) %>% unlist %>% as.integer()-1
-
+y_train = train %>% select(isFraud) %>% unlist %>% as.numeric()-1
+y_test = test %>% select(isFraud) %>% unlist %>% as.factor()
 
 xgbGrid <- expand.grid(nrounds = c(100, 200),
                        max_depth = c(10, 15, 20, 25),
@@ -57,22 +68,17 @@ xgb_trcontrol = trainControl(
   returnData = FALSE
 )
 
-xgb_fit = xgboost(data = train_dummy, label = y, max_depth = 2, 
+xgb_fit = xgboost(data = train_dummy, label = y_train, max_depth = 2, 
                   eta = 1, nthread = 2, nrounds = 2, objective = "binary:logistic")
 
-pred <- predict(xgb_fit, train_dummy)
-pred = ifelse(pred > 0.5, 1, 0)
+pred_out <- predict(xgb_fit, test_dummy)
+pred = ifelse(pred_out > 0.5, 1, 0)
 pred = as.factor(pred)
-act = as.factor(y)
-confusionMatrix(pred, act)
+confusionMatrix(pred, y_test)
 
-pred = prediction(as.numeric(pred), as.numeric(y))
+pred = prediction(as.numeric(pred), as.numeric(y_test))
 auc = performance(pred, measure = "auc")
 
 perf = performance(pred,"tpr","fpr")
 
 plot(perf, main = auc@y.values[[1]], colorize = TRUE)
-
-xgb_imp = varImp(xgb_fit)
-plot(xgb_imp)
-xgb.plot.importance(xgb_fit)
